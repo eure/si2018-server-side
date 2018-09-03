0cb32712522607e07c252f7031e5d51da0939db4
Prog24 <prog24.net@gmail.com>
2018-09-03 17:38:04 +0900

    create get profile api

--- restapi/operations/user/user.go
+++ restapi/operations/user/user.go
@@ -3,7 +3,10 @@ package user
 import (
 	"github.com/go-openapi/runtime/middleware"
 
+	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
+	"strings"
+	"strconv"
 )
 
 func GetUsers(p si.GetUsersParams) middleware.Responder {
@@ -11,7 +14,34 @@ func GetUsers(p si.GetUsersParams) middleware.Responder {
 }
 
 func GetProfileByUserID(p si.GetProfileByUserIDParams) middleware.Responder {
-	return si.NewGetProfileByUserIDOK()
+	if !(strings.HasPrefix(p.Token, "USERTOKEN")) || !(strings.HasSuffix(p.Token, strconv.FormatInt(p.UserID, 10))) {
+		return si.NewGetProfileByUserIDUnauthorized().WithPayload(
+			&si.GetProfileByUserIDUnauthorizedBody{
+				Code:    "401",
+				Message: "Token Is Invalid",
+			})
+	}
+
+	r := repositories.NewUserRepository()
+	ent, err := r.GetByUserID(p.UserID)
+
+	if err != nil {
+		return si.NewGetUsersInternalServerError().WithPayload(
+			&si.GetUsersInternalServerErrorBody{
+				Code: "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if ent == nil {
+		return si.NewGetTokenByUserIDNotFound().WithPayload(
+			&si.GetTokenByUserIDNotFoundBody{
+				Code: "404",
+				Message: "User Not Found",
+			})
+	}
+
+	sEnt := ent.Build()
+	return si.NewGetProfileByUserIDOK().WithPayload(&sEnt)
 }
 
 func PutProfile(p si.PutProfileParams) middleware.Responder {
