d4ffa7762a3dcf563c7359df017c167fcbda714d
k-nasa <htilcs1115@gmail.com>
2018-09-04 08:36:22 +0900

    $gofmt -s -w ./

--- restapi/operations/user/user.go
+++ restapi/operations/user/user.go
@@ -4,42 +4,52 @@ import (
 	"github.com/go-openapi/runtime/middleware"
 
 	"github.com/eure/si2018-server-side/entities"
-  "github.com/eure/si2018-server-side/repositories"
-  si "github.com/eure/si2018-server-side/restapi/summerintern"
+	"github.com/eure/si2018-server-side/repositories"
+	si "github.com/eure/si2018-server-side/restapi/summerintern"
 )
 
 func GetUsers(p si.GetUsersParams) middleware.Responder {
-  //FIXME Token認証は共通化したい
-  r := repositories.NewUserTokenRepository()
+	//FIXME Token認証は共通化したい
+	r := repositories.NewUserTokenRepository()
 
-  ent, err := r.GetByToken(p.Token)
+	ent, err := r.GetByToken(p.Token)
 
-  if err != nil { return getUsersInteralServerErrorResponse("Internal Server Error") }
-  if ent == nil { return getUsersUnauthorizedResponse("Your Token Is Invalid") }
+	if err != nil {
+		return getUsersInteralServerErrorResponse("Internal Server Error")
+	}
+	if ent == nil {
+		return getUsersUnauthorizedResponse("Your Token Is Invalid")
+	}
 
-  userToken := ent.Build()
-  userID := userToken.UserID
+	userToken := ent.Build()
+	userID := userToken.UserID
 
-  userLikeRepository := repositories.NewUserLikeRepository()
-  exclusionIds, err := userLikeRepository.FindLikeAll(userID)
+	userLikeRepository := repositories.NewUserLikeRepository()
+	exclusionIds, err := userLikeRepository.FindLikeAll(userID)
 
-  if err != nil { return getUsersInteralServerErrorResponse("Internal Server Error") }
+	if err != nil {
+		return getUsersInteralServerErrorResponse("Internal Server Error")
+	}
 
-  userRepository := repositories.NewUserRepository()
-  userEnt, err  := userRepository.GetByUserID(userID)
-  if err != nil { return getUsersInteralServerErrorResponse("Internal Server Error") }
+	userRepository := repositories.NewUserRepository()
+	userEnt, err := userRepository.GetByUserID(userID)
+	if err != nil {
+		return getUsersInteralServerErrorResponse("Internal Server Error")
+	}
 
-  // int64になっているのでcastする必要がある
-  limit := int(p.Limit)
-  offset := int(p.Offset)
-  gender := userEnt.GetOppositeGender()
+	// int64になっているのでcastする必要がある
+	limit := int(p.Limit)
+	offset := int(p.Offset)
+	gender := userEnt.GetOppositeGender()
 
-  var usersEnt entities.Users
-  usersEnt, err = userRepository.FindUsers(limit, offset, gender, exclusionIds)
-  if err != nil { return getUsersInteralServerErrorResponse("Internal Server Error") }
+	var usersEnt entities.Users
+	usersEnt, err = userRepository.FindUsers(limit, offset, gender, exclusionIds)
+	if err != nil {
+		return getUsersInteralServerErrorResponse("Internal Server Error")
+	}
 
-  users := usersEnt.Build()
-  return si.NewGetUsersOK().WithPayload(users)
+	users := usersEnt.Build()
+	return si.NewGetUsersOK().WithPayload(users)
 }
 
 func GetProfileByUserID(p si.GetProfileByUserIDParams) middleware.Responder {
@@ -51,17 +61,17 @@ func PutProfile(p si.PutProfileParams) middleware.Responder {
 }
 
 func getUsersInteralServerErrorResponse(message string) middleware.Responder {
-  return si.NewGetUsersInternalServerError().WithPayload(
-    &si.GetUsersInternalServerErrorBody {
-      Code:    "500",
-      Message: message,
-  })
+	return si.NewGetUsersInternalServerError().WithPayload(
+		&si.GetUsersInternalServerErrorBody{
+			Code:    "500",
+			Message: message,
+		})
 }
 
 func getUsersUnauthorizedResponse(message string) middleware.Responder {
-  return si.NewGetUsersUnauthorized().WithPayload(
-    &si.GetUsersUnauthorizedBody{
-      Code:    "401",
-      Message: "Your Token Is Invalid",
-    })
-  }
+	return si.NewGetUsersUnauthorized().WithPayload(
+		&si.GetUsersUnauthorizedBody{
+			Code:    "401",
+			Message: "Your Token Is Invalid",
+		})
+}
