f6eadb9fe0f648a01669723664ab97baa6322315
秋田　海人 <kaitoakita07071@gmail.com>
2018-09-03 23:20:40 +0900

    ユーザーの詳細情報を表示するapi

--- restapi/operations/user/user.go
+++ restapi/operations/user/user.go
@@ -3,6 +3,7 @@ package user
 import (
 	"github.com/go-openapi/runtime/middleware"
 
+	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 )
 
@@ -11,7 +12,38 @@ func GetUsers(p si.GetUsersParams) middleware.Responder {
 }
 
 func GetProfileByUserID(p si.GetProfileByUserIDParams) middleware.Responder {
-	return si.NewGetProfileByUserIDOK()
+	r := repositories.NewUserRepository()
+
+	//token vaildate
+	// v := vaildate.ValidateToken(p.UserID, p.Token)
+	//
+	// if v != true {
+	// 	return si.NewGetProfileByUserIDTokenIsInvalid().WithPayload(
+	// 		&si.GetProfileByUserIDTokenIsInvalid{
+	// 			Code:  "401",
+	// 			Message: "Token Is Invalid",
+	// 		})
+	// }
+
+	ent, err := r.GetByUserID(p.UserID)
+
+	if err != nil {
+		return si.NewGetProfileByUserIDInternalServerError().WithPayload(
+			&si.GetProfileByUserIDInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if ent == nil {
+		return si.NewGetProfileByUserIDNotFound().WithPayload(
+			&si.GetProfileByUserIDNotFoundBody{
+				Code:    "404",
+				Message: "User Token Not Found",
+			})
+	}
+
+	sEnt := ent.Build()
+	return si.NewGetProfileByUserIDOK().WithPayload(&sEnt)
 }
 
 func PutProfile(p si.PutProfileParams) middleware.Responder {
