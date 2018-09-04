15b3b6fac9b374bf69a385e898c9c812be4e0083
eureka <eureka@PC11509005.local>
2018-09-04 21:16:29 +0900

    add token validation (as string)

--- restapi/operations/util/util.go
+++ restapi/operations/util/util.go
@@ -1,6 +1,10 @@
 package util
 
 import (
+	"strings"
+	"strconv"
+	"errors"
+	"fmt"
 	"github.com/eure/si2018-server-side/repositories"
 )
 
@@ -14,14 +18,29 @@ func GetIDByToken(token string) (int64, error) {
 
 	return ut.UserID, nil
 }
-/*
+
 func ValidateToken(token string) error {
 	// 文字列として正しいか
+	if !isTokenStrValid(token) {
+		return errors.New("Invalid token string ('USERTOKEN{id}' is required)")
+	}
 	// userが存在するか
+	return nil
 }
 
 func isTokenStrValid(token string) bool {
+	if !(strings.HasPrefix(token, "USERTOKEN")) {
+		return false
+	}
+
+	id, err := strconv.Atoi(token[9:])
+	if !(id > 0) || (err != nil) {
+		return false
+	}
+
+	return true
 }
-
-
+/*
+func userExists(token string) bool {
+}
 */
