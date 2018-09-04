7d69fb8645a8b6ad665e0710c087bc928e24d1a4
秋田　海人 <kaitoakita07071@gmail.com>
2018-09-05 00:28:58 +0900

    自分が既にLikeをしているUserが欲しかったのでFindLikeOnleyを作成

--- repositories/user_like_repository.go
+++ repositories/user_like_repository.go
@@ -38,6 +38,27 @@ func (r *UserLikeRepository) FindLikeAll(userID int64) ([]int64, error) {
 	return ids, nil
 }
 
+// 自分が既にLikeしている全てのUserのIDを返す
+func (r *UserLikeRepository) FindLikeOnley(userID int64) ([]int64, error) {
+	var likes []entities.UserLike
+	var ids []int64
+
+	err := engine.Where("user_id = ?", userID).Find(&likes)
+	if err != nil {
+		return ids, err
+	}
+
+	for _, l := range likes {
+		if l.UserID == userID {
+			ids = append(ids, l.PartnerID)
+			continue
+		}
+		ids = append(ids, l.UserID)
+	}
+
+	return ids, nil
+}
+
 // いいねを1件取得する.
 // userIDはいいねを送った人, partnerIDはいいねを受け取った人.
 func (r *UserLikeRepository) GetLikeBySenderIDReceiverID(userID, partnerID int64) (*entities.UserLike, error) {
