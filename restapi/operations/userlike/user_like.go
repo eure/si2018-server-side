fe43870f80f757c519f955b20a273301d8533e04
jon <kawadu1221@gmail.com>
2018-09-04 18:40:24 +0900

    add get userlike

--- restapi/operations/userlike/user_like.go
+++ restapi/operations/userlike/user_like.go
@@ -3,7 +3,7 @@ package userlike
 import (
 	"fmt"
 
-	"github.com/eure/si2018-server-side/models"
+	"github.com/eure/si2018-server-side/entities"
 	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 	"github.com/go-openapi/runtime/middleware"
@@ -12,43 +12,46 @@ import (
 func GetLikes(p si.GetLikesParams) middleware.Responder {
 	nulr := repositories.NewUserLikeRepository()
 	nutr := repositories.NewUserTokenRepository()
-	//nur := repositories.NewUserRepository()
+	nur := repositories.NewUserRepository()
 	numr := repositories.NewUserMatchRepository()
 
 	usrid, err := nutr.GetByToken(p.Token)
 	if err != nil {
-		fmt.Println(err)
 		return si.NewGetUsersInternalServerError().WithPayload(
 			&si.GetUsersInternalServerErrorBody{
 				Code:    "404",
 				Message: "User Token Not Found",
 			})
 	}
-	fmt.Println(usrid)
+	// find already matching user
 	match, err := numr.FindAllByUserID(usrid.UserID)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(match)
 
+	// find recive like except already matching user
 	usrs, err := nulr.FindGotLikeWithLimitOffset(usrid.UserID, int(p.Limit), int(p.Offset), match)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(usrs)
-	sample, _ := nulr.FindLikeAll(usrid.UserID)
-	fmt.Println(sample)
 	var userids []int64
-	for i := 0; 0 < len(usrs); i++ {
+	for i := 0; i < len(usrs); i++ {
 		userids = append(userids, usrs[i].UserID)
 	}
-	ent, err := nulr.FindAllLikeUserResponse(sample)
-	var ud []*models.LikeUserResponse
-	for i := 0; i < len(ent); i++ {
-		us := ent[i].Build()
-		ud = append(ud, &us)
+	//jimae
+	ent, err := nur.FindByIDs(userids)
+	if err != nil {
+		fmt.Println(err)
 	}
-	return si.NewGetLikesOK().WithPayload(ud)
+	var likeuserresp entities.LikeUserResponses
+	for _, val := range ent {
+		var tmp = entities.LikeUserResponse{}
+		tmp.ApplyUser(val)
+		likeuserresp = append(likeuserresp, tmp)
+	}
+	sEnt := likeuserresp.Build()
+
+	return si.NewGetLikesOK().WithPayload(sEnt)
 }
 
 func PostLike(p si.PostLikeParams) middleware.Responder {
