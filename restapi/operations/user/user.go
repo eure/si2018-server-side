b0ff8c17602f227a475c52bd0f54a901d5cf7226
秋田　海人 <kaitoakita07071@gmail.com>
2018-09-04 10:01:03 +0900

    探すAPIの処理

--- restapi/operations/user/user.go
+++ restapi/operations/user/user.go
@@ -2,13 +2,38 @@ package user
 
 import (
 	"github.com/go-openapi/runtime/middleware"
-
+	"fmt"
+	"github.com/eure/si2018-server-side/entities"
 	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 )
 
 func GetUsers(p si.GetUsersParams) middleware.Responder {
-	return si.NewGetUsersOK()
+	t := repositories.NewUserTokenRepository()
+	l := repositories.NewUserLikeRepository()
+	u := repositories.NewUserRepository()
+
+	//トークンからユーザーidを取得する為に利用
+	ut, _ := t.GetByToken(p.Token)
+	//いいねをすでに送っている人を取得
+	ids, _ := l.FindLikeAll(ut.UserID)
+	//男か女か情報が必要なのでユーザーの情報を取得する
+	user, _ := u.GetByUserID(ut.UserID)
+
+	//ユーザーが男だった時に検索するユーザーを女とする
+	var g string
+	if user.Gender == "M" {
+		g = "F"
+	}else{
+		g = "M"
+	}
+	//明示的に型宣言
+	var f entities.Users
+	//探す処理
+	f, _ = u.FindWithCondition(int(p.Limit), int(p.Offset), g, ids)
+
+	sEnt := f.Build()
+	return si.NewGetUsersOK().WithPayload(sEnt)
 }
 
 func GetProfileByUserID(p si.GetProfileByUserIDParams) middleware.Responder {
