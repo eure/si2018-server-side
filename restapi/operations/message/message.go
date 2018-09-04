05d654c4a0f0998da2ea2e7ca384edb709024845
Prog24 <prog24.net@gmail.com>
2018-09-04 16:18:22 +0900

    fix get messages

--- restapi/operations/message/message.go
+++ restapi/operations/message/message.go
@@ -7,6 +7,7 @@ import (
 	"github.com/eure/si2018-server-side/entities"
 	"github.com/go-openapi/strfmt"
 	"time"
+	"strings"
 )
 
 func PostMessage(p si.PostMessageParams) middleware.Responder {
@@ -45,24 +46,40 @@ func PostMessage(p si.PostMessageParams) middleware.Responder {
 }
 
 func GetMessages(p si.GetMessagesParams) middleware.Responder {
-	messageR := repositories.NewUserMessageRepository()
-
+	// Tokenの形式がおかしい -> 401
+	if !(strings.HasPrefix(p.Token, "USERTOKEN"))  {
+		return si.NewGetMessagesUnauthorized().WithPayload(
+			&si.GetMessagesUnauthorizedBody{
+				Code:    "401",
+				Message: "Token Is Invalid",
+			})
+	}
+	// Tokenのユーザが存在しない -> 400 Bad Request
 	tokenR := repositories.NewUserTokenRepository()
 	tokenEnt, err := tokenR.GetByToken(p.Token)
 	if err != nil {
-		return si.NewGetUsersInternalServerError().WithPayload(
-			&si.GetUsersInternalServerErrorBody{
-				Code    : "500",
-				Message : "Internal Server Error",
+		return si.NewGetMessagesInternalServerError().WithPayload(
+			&si.GetMessagesInternalServerErrorBody{
+				Code: "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if tokenEnt == nil{
+		return si.NewGetMessagesBadRequest().WithPayload(
+			&si.GetMessagesBadRequestBody{
+				Code: "400",
+				Message: "Bad Request",
 			})
 	}
 
-	var responseEnt entities.UserMessages
+	// TODO: Partnerかどうかのチェックは必要？
+	messageR := repositories.NewUserMessageRepository()
+	var responseEnts entities.UserMessages
 	messages, _ := messageR.GetMessages(tokenEnt.UserID, p.UserID, int(*p.Limit), p.Latest, p.Oldest)
 	for _, message := range messages {
-		responseEnt = append(responseEnt, message)
-		}
+		responseEnts = append(responseEnts, message)
+	}
 
-	responseData := responseEnt.Build()
+	responseData := responseEnts.Build()
 	return si.NewGetMessagesOK().WithPayload(responseData)
 }
