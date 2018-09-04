b431cbef635f1a116d02f3f60d3bbb91201f7b23
Prog24 <prog24.net@gmail.com>
2018-09-04 09:45:08 +0900

    create post message

--- restapi/operations/message/message.go
+++ restapi/operations/message/message.go
@@ -3,10 +3,46 @@ package message
 import (
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/eure/si2018-server-side/repositories"
+	"github.com/eure/si2018-server-side/entities"
+	"github.com/go-openapi/strfmt"
+	"time"
 )
 
 func PostMessage(p si.PostMessageParams) middleware.Responder {
-	return si.NewPostMessageOK()
+	tokenR := repositories.NewUserTokenRepository()
+	tokenEnt, err := tokenR.GetByToken(p.Params.Token)
+	if err != nil {
+		return si.NewGetUsersInternalServerError().WithPayload(
+			&si.GetUsersInternalServerErrorBody{
+				Code    : "500",
+				Message : "Internal Server Error",
+			})
+	}
+
+	messageR := repositories.NewUserMessageRepository()
+	tmp := entities.UserMessage{
+		UserID: tokenEnt.UserID,
+		PartnerID: p.UserID,
+		Message: p.Params.Message,
+		CreatedAt: strfmt.DateTime(time.Now()),
+		UpdatedAt: strfmt.DateTime(time.Now()),
+	}
+	err = messageR.Create(tmp)
+	if err != nil {
+		return si.NewGetUsersInternalServerError().WithPayload(
+			&si.GetUsersInternalServerErrorBody{
+				Code    : "500",
+				Message : "Internal Server Error",
+			})
+	}
+
+
+	return si.NewPostMessageOK().WithPayload(
+		&si.PostMessageOKBody{
+			Code: "200",
+			Message: "OK",
+		})
 }
 
 func GetMessages(p si.GetMessagesParams) middleware.Responder {
