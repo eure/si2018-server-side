4d568ae9ab2ce2cc7c9fc4713ed86e3905631821
jon <kawadu1221@gmail.com>
2018-09-04 19:43:36 +0900

    add get match api

--- restapi/operations/usermatch/user_match.go
+++ restapi/operations/usermatch/user_match.go
@@ -3,6 +3,7 @@ package usermatch
 import (
 	"fmt"
 
+	"github.com/eure/si2018-server-side/entities"
 	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 	"github.com/go-openapi/runtime/middleware"
@@ -11,12 +12,25 @@ import (
 func GetMatches(p si.GetMatchesParams) middleware.Responder {
 	nutr := repositories.NewUserTokenRepository()
 	numr := repositories.NewUserMatchRepository()
+	nur := repositories.NewUserRepository()
 	user, err := nutr.GetByToken(p.Token)
 	ent, err := numr.FindByUserIDWithLimitOffset(user.UserID, int(p.Limit), int(p.Offset))
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(ent)
+	var userids []int64
+	for _, val := range ent {
+		userids = append(userids, val.PartnerID)
+	}
+	fmt.Println(userids)
+	ents, _ := nur.FindByIDs(userids)
+	var allmatches entities.MatchUserResponses
+	for _, val := range ents {
+		var tmp = entities.MatchUserResponse{}
+		tmp.ApplyUser(val)
+		allmatches = append(allmatches, tmp)
+	}
+	sEnt := allmatches.Build()
 
-	return si.NewGetMatchesOK()
+	return si.NewGetMatchesOK().WithPayload(sEnt)
 }
