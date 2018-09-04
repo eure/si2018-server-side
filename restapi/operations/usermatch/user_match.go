2889b98510726cf2e1a1336ec6a0d1dd21d7afbd
jon <kawadu1221@gmail.com>
2018-09-04 17:19:16 +0900

    modify lately

--- restapi/operations/usermatch/user_match.go
+++ restapi/operations/usermatch/user_match.go
@@ -1,10 +1,22 @@
 package usermatch
 
 import (
+	"fmt"
+
+	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 	"github.com/go-openapi/runtime/middleware"
 )
 
 func GetMatches(p si.GetMatchesParams) middleware.Responder {
+	nutr := repositories.NewUserTokenRepository()
+	numr := repositories.NewUserMatchRepository()
+	user, err := nutr.GetByToken(p.Token)
+	ent, err := numr.FindByUserIDWithLimitOffset(user.UserID, int(p.Limit), int(p.Offset))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(ent)
+
 	return si.NewGetMatchesOK()
 }
