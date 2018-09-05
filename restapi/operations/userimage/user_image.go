a1a3cb6e1ad88803c38cc97e19308ca2ca4944e6
eureka <eureka@PC11509005.local>
2018-09-05 16:58:33 +0900

    add PostImage

--- restapi/operations/userimage/user_image.go
+++ restapi/operations/userimage/user_image.go
@@ -6,16 +6,58 @@ import (
 	_ "image/png"
 
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
-	//"github.com/eure/si2018-server-side/restapi/operations/util"
+	"github.com/eure/si2018-server-side/restapi/operations/util"
+	"github.com/eure/si2018-server-side/repositories"
+	"github.com/eure/si2018-server-side/entities"
 	"github.com/go-openapi/runtime/middleware"
+	strfmt "github.com/go-openapi/strfmt"
+	"os"
+	"fmt"
+	"strconv"
+	"time"
 )
 
 func PostImage(p si.PostImagesParams) middleware.Responder {
-	/*token := p.Params.Token
-	img := p.Params.Image
+	token := p.Params.Token
+	img := p.Params.Image //Image strfmt.Base64 -> type Base64 []byte
+
+	err := util.ValidateToken(token)
+	if err != nil {
+		return si.NewPostImagesUnauthorized().WithPayload(
+			&si.PostImagesUnauthorizedBody{
+				Code:    "401",
+				Message: "Token Is Invalid",
+			})
+	}
+
+	id, _ := util.GetIDByToken(token)
+
+	ap := os.Getenv("ASSETS_PATH")
+	filename := "icon" + strconv.Itoa(int(id)) + ".png" /* TODO filetype */
+
+	f, err := os.Create(ap + filename)
+	if err != nil {
+		fmt.Println(err)
+		return si.NewPostImagesOK()
+	}
+	defer f.Close()
+	f.Write(img)
+
+	ui := entities.UserImage{}
+	ui.UserID = id
+	ui.Path = ap + filename
 
 	r := repositories.NewUserImageRepository()
+	oldimg, _ := r.GetByUserID(id)
+	fmt.Println(oldimg)
+	err = r.Update(ui) /* TODO check existance? */
+	fmt.Println(err)
+	newimg, err := r.GetByUserID(id)
+	time.Sleep(1 * time.Second)
+	fmt.Println(newimg)
 
-	res := si.PostImagesOKBody{}*/
-	return si.NewPostImagesOK()
+	return si.NewPostImagesOK().WithPayload(
+		&si.PostImagesOKBody{
+			ImageURI: strfmt.URI(ap + filename),
+		})
 }
