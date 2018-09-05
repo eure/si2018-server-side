237cdcb2cbd72b1061d6fc5b86b8588c95d46260
jon <kawadu1221@gmail.com>
2018-09-05 16:54:29 +0900

    add post message

--- restapi/operations/message/message.go
+++ restapi/operations/message/message.go
@@ -2,16 +2,50 @@ package message
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/eure/si2018-server-side/entities"
 	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/strfmt"
 )
 
 func PostMessage(p si.PostMessageParams) middleware.Responder {
+	numr := repositories.NewUserMessageRepository()
+	nutr := repositories.NewUserTokenRepository()
+	//nur := repositories.NewUserRepository()
+	numar := repositories.NewUserMatchRepository()
+	// get my user id
+	usertoken, err := nutr.GetByToken(p.Params.Token)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	return si.NewPostMessageOK()
+	// validate already matching
+	existmatch, err := numar.Get(usertoken.UserID, p.UserID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if existmatch == nil {
+		fmt.Println(err)
+	}
+
+	user := entities.UserMessage{
+		UserID:    usertoken.UserID,
+		PartnerID: p.UserID,
+		Message:   p.Params.Message,
+		CreatedAt: strfmt.DateTime(time.Now()),
+		UpdatedAt: strfmt.DateTime(time.Now()),
+	}
+
+	numr.Create(user)
+
+	return si.NewPostMessageOK().WithPayload(
+		&si.PostMessageOKBody{
+			Code:    "200",
+			Message: "OK",
+		})
 }
 
 func GetMessages(p si.GetMessagesParams) middleware.Responder {
