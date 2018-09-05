6906bc8eabe0ec68fc9252e5d3e4ea9b3fce8f8a
Rentearo Igari <>
2018-09-05 13:01:24 +0900

    メッセージを取得するところまで実装した

--- restapi/operations/message/message.go
+++ restapi/operations/message/message.go
@@ -3,6 +3,7 @@ package message
 import (
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/eure/si2018-server-side/repositories"
 )
 
 func PostMessage(p si.PostMessageParams) middleware.Responder {
@@ -10,5 +11,52 @@ func PostMessage(p si.PostMessageParams) middleware.Responder {
 }
 
 func GetMessages(p si.GetMessagesParams) middleware.Responder {
+
+	// レポジトリを初期化する
+	tokenR := repositories.NewUserTokenRepository()
+	usermessageR := repositories.NewUserMessageRepository()
+
+	// トークンを検索する
+	tokenEnt, err := tokenR.GetByToken(p.Token)
+
+	// 401エラー
+	if tokenEnt == nil {
+		si.NewGetMessagesUnauthorized().WithPayload(
+			&si.GetMessagesUnauthorizedBody{
+				Code: "401",
+				Message: "Token is invalid",
+			})
+	}
+
+	// 500エラー
+	if err != nil {
+		si.NewGetMessagesInternalServerError().WithPayload(
+			&si.GetMessagesInternalServerErrorBody{
+				Code: "500",
+				Message: "Internal Server Error",
+			})
+	}
+
+	// limitのデフォルト値を100に設定する
+	var limit int
+	if &p.Limit == nil {
+		limit = 100
+	} else {
+		limit = int(*p.Limit)
+	}
+
+	messageEnts, err := usermessageR.GetMessages(tokenEnt.UserID, p.UserID, limit, p.Latest, p.Oldest)
+
+	// 500エラー
+	if err != nil {
+		si.NewGetMessagesInternalServerError().WithPayload(
+			&si.GetMessagesInternalServerErrorBody{
+				Code: "500",
+				Message: "Internal Server Error",
+			})
+	}
+
+
+
 	return si.NewGetMessagesOK()
 }
