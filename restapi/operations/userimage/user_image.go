a71ebc297766e6dddb028be9bcc6d2f5b1237a5d
jon <kawadu1221@gmail.com>
2018-09-05 16:55:07 +0900

    add post image

--- restapi/operations/userimage/user_image.go
+++ restapi/operations/userimage/user_image.go
@@ -1,14 +1,72 @@
 package userimage
 
 import (
+	"bytes"
+	"fmt"
+	"image"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
+	"os"
+	"time"
 
+	"github.com/eure/si2018-server-side/entities"
+	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/strfmt"
 )
 
 func PostImage(p si.PostImagesParams) middleware.Responder {
-	return si.NewPostImagesOK()
+	nuir := repositories.NewUserImageRepository()
+	nutr := repositories.NewUserTokenRepository()
+
+	usertkn, err := nutr.GetByToken(p.Params.Token)
+	if err != nil {
+		fmt.Println("token err")
+	}
+
+	// Define save directory
+	writeimage := p.Params.Image
+
+	pathdir := "assets/" + usertkn.Token
+	checkext := bytes.NewReader(writeimage)
+	_, extension, err := image.DecodeConfig(checkext)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// check image extension
+	switch extension {
+	case "png":
+		pathdir += ".png"
+	case "jpg":
+		pathdir += ".jpg"
+	}
+	file, err := os.Create(pathdir)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	//write picture
+	file.Write(writeimage)
+	userimage := entities.UserImage{
+		UserID:    usertkn.UserID,
+		Path:      pathdir,
+		CreatedAt: strfmt.DateTime(time.Now()),
+		UpdatedAt: strfmt.DateTime(time.Now()),
+	}
+
+	err = nuir.Update(userimage)
+	if err != nil {
+		fmt.Println(err)
+	}
+	userimg, err := nuir.GetByUserID(usertkn.UserID)
+	if err != nil {
+		fmt.Println("not find your account")
+	}
+	fmt.Println(userimg)
+	return si.NewPostImagesOK().WithPayload(
+		&si.PostImagesOKBody{
+			ImageURI: strfmt.URI(pathdir),
+		})
 }
