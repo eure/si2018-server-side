dac41e0e344ba07f75562b9dadf19f44f02e87c7
秋田　海人 <kaitoakita07071@gmail.com>
2018-09-06 01:21:10 +0900

    user_match.goにエラーハンドリングを追加

--- restapi/operations/usermatch/user_match.go
+++ restapi/operations/usermatch/user_match.go
@@ -1,10 +1,10 @@
 package usermatch
 
 import (
-	si "github.com/eure/si2018-server-side/restapi/summerintern"
-	"github.com/eure/si2018-server-side/repositories"
-	"github.com/go-openapi/runtime/middleware"
 	"github.com/eure/si2018-server-side/entities"
+	"github.com/eure/si2018-server-side/repositories"
+	si "github.com/eure/si2018-server-side/restapi/summerintern"
+	"github.com/go-openapi/runtime/middleware"
 )
 
 func GetMatches(p si.GetMatchesParams) middleware.Responder {
@@ -12,22 +12,54 @@ func GetMatches(p si.GetMatchesParams) middleware.Responder {
 	m := repositories.NewUserMatchRepository()
 	u := repositories.NewUserRepository()
 
-	ut, _ := t.GetByToken(p.Token)
+	// ユーザーID取得用
+	token, _ := t.GetByToken(p.Token)
 
 	// マッチ済みのユーザーを取得
-	match, _ := m.FindByUserIDWithLimitOffset(ut.UserID, int(p.Limit), int(p.Offset))
+	match, err := m.FindByUserIDWithLimitOffset(token.UserID, int(p.Limit), int(p.Offset))
+	if err != nil {
+		return si.NewGetMatchesInternalServerError().WithPayload(
+			&si.GetMatchesInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if match == nil {
+		return si.NewGetMatchesBadRequest().WithPayload(
+			&si.GetMatchesBadRequestBody{
+				Code:    "400",
+				Message: "Bad Request",
+			})
+	}
 
 	// ApplyUserの形にしてBuildしないといけないので
 	// ApplyUserの型に変換する
 	var mr entities.MatchUserResponses
-	for _, matchUser := range match{
+	for _, matchUser := range match {
 		// 構造体の初期化(単体)
 		r := entities.MatchUserResponse{}
-		user, _ := u.GetByUserID(matchUser.PartnerID)
+
+		// Userの情報を取得
+		user, err := u.GetByUserID(matchUser.PartnerID)
+		if err != nil {
+			return si.NewGetMatchesInternalServerError().WithPayload(
+				&si.GetMatchesInternalServerErrorBody{
+					Code:    "500",
+					Message: "Internal Server Error",
+				})
+		}
+		if user == nil {
+			return si.NewGetMatchesBadRequest().WithPayload(
+				&si.GetMatchesBadRequestBody{
+					Code:    "400",
+					Message: "Bad Request",
+				})
+		}
+
 		r.ApplyUser(*user)
 		mr = append(mr, r)
 	}
 
-  sEnt := mr.Build()
+	sEnt := mr.Build()
 	return si.NewGetMatchesOK().WithPayload(sEnt)
 }
