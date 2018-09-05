55aa96f66ab905fcd91d2f774074423ed01c36b5
NoritomoTaba <minamiedamame0328@gmail.com>
2018-09-06 00:48:08 +0900

    implemented POST API for sending message

--- restapi/operations/message/message.go
+++ restapi/operations/message/message.go
@@ -3,12 +3,56 @@ package message
 import (
 	"github.com/eure/si2018-server-side/entities"
 	"github.com/eure/si2018-server-side/repositories"
-	si "github.com/eure/si2018-server-side/restapi/summerintern"
+	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/runtime/middleware"
+	
+	"time"
+	si "github.com/eure/si2018-server-side/restapi/summerintern"
 )
 
 func PostMessage(p si.PostMessageParams) middleware.Responder {
-	return si.NewPostMessageOK()
+	r := repositories.NewUserMessageRepository()
+	t := repositories.NewUserTokenRepository()
+	//u := repositories.NewUserRepository()
+	m := repositories.NewUserMatchRepository()
+	
+	// 作成するメッセージの宣言
+	var message entities.UserMessage
+	
+	// Tokenの取得, 送信元UserIDの宣言
+	FromUserToken := p.Params.Token
+	FromUser, err := t.GetByToken(FromUserToken)
+	if err != nil {
+		return outPutPostStatus(500)
+	}
+	FromUserID := FromUser.UserID
+	
+	// メッセージ送信元UserIDの宣言
+	ToUserID := p.UserID
+	
+	// メッセージの作成
+	message.UserID = ToUserID
+	message.PartnerID = FromUserID
+	message.Message = p.Params.Message
+	message.CreatedAt = strfmt.DateTime(time.Now())
+	message.UpdatedAt = strfmt.DateTime(time.Now())
+	
+	// マッチ済みかどうか
+	userMatch , err := m.Get(FromUserID,ToUserID)
+	if userMatch != nil {
+		r.Create(message)
+	}
+	
+	userMatch , err = m.Get(ToUserID, FromUserID)
+	if userMatch != nil {
+		r.Create(message)
+	}
+	
+	return si.NewPostMessageOK().WithPayload(
+		&si.PostMessageOKBody{
+			Code:    "200",
+			Message: "OK",
+		})
 }
 
 func GetMessages(p si.GetMessagesParams) middleware.Responder {
@@ -38,4 +82,22 @@ func GetMessages(p si.GetMessagesParams) middleware.Responder {
 
 	sEnt := messages.Build()
 	return si.NewGetMessagesOK().WithPayload(sEnt)
+}
+
+func outPutPostStatus (num int) middleware.Responder {
+	switch num {
+	case 500:
+		return si.NewPostMessageInternalServerError().WithPayload(
+			&si.PostMessageInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	case 400:
+		return si.NewPostMessageBadRequest().WithPayload(
+			&si.PostMessageBadRequestBody{
+				Code:    "400",
+				Message: "Bad Request",
+			})
+	}
+	return nil
 }
