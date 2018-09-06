b9ae95164c0fab355b0c8a5582fc5bf3d0a544d1
NoritomoTaba <minamiedamame0328@gmail.com>
2018-09-06 17:32:35 +0900

    modified variable name,comment

--- restapi/operations/usermatch/user_match.go
+++ restapi/operations/usermatch/user_match.go
@@ -15,20 +15,61 @@ func GetMatches(p si.GetMatchesParams) middleware.Responder {
 	
 	// tokenから UserToken entitiesの取得 (Validation)
 	token := p.Token
-	loginUserToken , _ := t.GetByToken(token)
-
-	ent, _ := r.FindByUserIDWithLimitOffset(loginUserToken.UserID,int(p.Limit),int(p.Offset))
-
-	// applied メソッドによって変換されたUsersがほしい。
-	// とりあえずほしいから，格納先を用意してあげる。
-	var appliedusers entities.MatchUserResponses
-	for _,m := range ent {
-		var applied = entities.MatchUserResponse{}
-		matcheduser , _ := u.GetByUserID(m.PartnerID)
-		applied.ApplyUser(*matcheduser)
-		appliedusers = append (appliedusers, applied)
+	loginUserToken , err := t.GetByToken(token)
+	if err != nil {
+		return outPutGetStatus(500)
+	}
+	if loginUserToken == nil {
+		return outPutGetStatus(401)
 	}
 
-	sEtc := appliedusers.Build()
+	ent, err := r.FindByUserIDWithLimitOffset(loginUserToken.UserID,int(p.Limit),int(p.Offset))
+	if err != nil {
+		return outPutGetStatus(500)
+	}
+	if ent == nil {
+		return outPutGetStatus(400)
+	}
+	
+	// applied メソッドによって変換されたUsersがほしい。
+	// とりあえずほしいから，格納先を用意してあげる。
+	var appliedUsers entities.MatchUserResponses
+	for _,m := range ent {
+		var applied = entities.MatchUserResponse{}
+		matchedUsers, err := u.GetByUserID(m.PartnerID)
+		if err != nil {
+			return outPutGetStatus(500)
+		}
+
+		applied.ApplyUser(*matchedUsers)
+		appliedUsers = append (appliedUsers, applied)
+	}
+
+	sEtc := appliedUsers.Build()
 	return si.NewGetMatchesOK().WithPayload(sEtc)
+}
+
+
+func outPutGetStatus(num int) middleware.Responder {
+	switch num {
+	case 500:
+		return si.NewGetMatchesInternalServerError().WithPayload(
+			&si.GetMatchesInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	case 401:
+		return si.NewGetMatchesUnauthorized().WithPayload(
+			&si.GetMatchesUnauthorizedBody{
+				Code:    "401",
+				Message: "Unauthorized (トークン認証に失敗)",
+			})
+	case 400:
+		return si.NewGetMatchesBadRequest().WithPayload(
+			&si.GetMatchesBadRequestBody{
+				Code:    "400",
+				Message: "Bad Request",
+			})
+	}
+	return nil
 }
