2ac573806723b0c1d537f59d8771a01beb35bf49
k-nasa <htilcs1115@gmail.com>
2018-09-06 10:59:14 +0900

    add/ 画像保存できるようにした

--- restapi/operations/userimage/user_image.go
+++ restapi/operations/userimage/user_image.go
@@ -1,16 +1,58 @@
 package userimage
 
 import (
+	"fmt"
+
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
 
+	"github.com/eure/si2018-server-side/repositories"
 	si "github.com/eure/si2018-server-side/restapi/summerintern"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/strfmt"
 )
 
 func PostImage(p si.PostImagesParams) middleware.Responder {
-	return si.NewPostImagesOK()
+	userTokenEnt, err := repositories.NewUserTokenRepository().GetByToken(p.Params.Token)
+	if err != nil {
+		return postImageInternalServerErrorResponse()
+	}
+	if userTokenEnt == nil {
+		return postImageUnauthorizedResponse()
+	}
+
+	userID := userTokenEnt.UserID
+
+	userImageRepository := repositories.NewUserImageRepository()
+	typeName, err := userImageRepository.ImageValidation(p.Params.Image)
+	if err != nil {
+		str := fmt.Sprintf("%v", err)
+		return postImageBadRequestResponse(str)
+	}
+
+	imagePath, err := userImageRepository.SaveImageAssets(p.Params.Image, userID, typeName)
+	if err != nil {
+		return postImageInternalServerErrorResponse()
+	}
+
+	userImage, err := userImageRepository.GetByUserID(userID)
+	if err != nil {
+		return postImageInternalServerErrorResponse()
+	}
+
+	userImage.Path = imagePath
+	err = userImageRepository.Update(*userImage)
+	if err != nil {
+		return postImageInternalServerErrorResponse()
+	}
+
+	return si.NewPostImagesOK().WithPayload(
+		&si.PostImagesOKBody{
+			ImageURI: strfmt.URI("https://si-2018-011.eure.jp/" + imagePath),
+		})
+}
+
 func postImageInternalServerErrorResponse() middleware.Responder {
 	return si.NewPostImagesInternalServerError().WithPayload(
 		&si.PostImagesInternalServerErrorBody{
