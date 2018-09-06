cbc992cabb7428adbcb67f130367eabb945f06b3
秋田　海人 <kaitoakita07071@gmail.com>
2018-09-06 17:39:23 +0900

    DBを何度も呼びたしていたのをリファクタリング

--- restapi/operations/usermatch/user_match.go
+++ restapi/operations/usermatch/user_match.go
@@ -46,31 +46,29 @@ func GetMatches(p si.GetMatchesParams) middleware.Responder {
 			})
 	}
 
+	// 明示的に型宣言
+	var mr entities.MatchUserResponses
+	var userIDs []int64
+
+	for _, matchUser := range match {
+		userIDs = append(userIDs, matchUser.UserID)
+	}
+	// マッチしているユーザーの情報をすべて取得する
+	users, err := u.FindByIDs(userIDs)
+	if err != nil {
+		return si.NewGetLikesInternalServerError().WithPayload(
+			&si.GetLikesInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+
 	// ApplyUserの形にしてBuildしないといけないので
 	// ApplyUserの型に変換する
-	var mr entities.MatchUserResponses
-	for _, matchUser := range match {
-		// 構造体の初期化(単体)
+	for _, user := range users {
+		// 構造体の初期化
 		r := entities.MatchUserResponse{}
-
-		// Userの情報を取得
-		user, err := u.GetByUserID(matchUser.PartnerID)
-		if err != nil {
-			return si.NewGetMatchesInternalServerError().WithPayload(
-				&si.GetMatchesInternalServerErrorBody{
-					Code:    "500",
-					Message: "Internal Server Error",
-				})
-		}
-		if user == nil {
-			return si.NewGetMatchesBadRequest().WithPayload(
-				&si.GetMatchesBadRequestBody{
-					Code:    "400",
-					Message: "Bad Request",
-				})
-		}
-
-		r.ApplyUser(*user)
+		r.ApplyUser(user)
 		mr = append(mr, r)
 	}
 
